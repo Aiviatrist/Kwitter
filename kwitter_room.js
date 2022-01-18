8f4944d2b9d0957ef0d7e7409ddcbecee37cb49c
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-01-18 17:21:06 +1000

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -24,7 +24,11 @@ firebase.initializeApp(firebaseConfig);
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       
-
-      //End code
+      var row="<div class='container' id="+Room_names+"onclick='redirectToRoomname(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML += row;
       });});}
 getData();
+function redirectToRoomname(name){
+      localStorage.setItem("roomname",name);
+      window.location ="kwitter_page.html";
+}
